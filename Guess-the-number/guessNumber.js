3a617ede427c4419ce48c9c82b28bdc486099bba
modassir_hussain <89156644+hmodassir@users.noreply.github.com>
2022-01-17 12:55:09 +0530

    Add files via upload

---
+++ Guess-the-number/guessNumber.js
@@ -0,0 +1,44 @@
+let final_number = Math.floor(Math.random()*100)+1
+let display_el = document.getElementById("display")
+let chance_el = document.getElementById("user_chance")
+let chance = 10
+let chance_text = "Chance Left: "
+chance_el.textContent = chance_text + chance
+
+
+
+function checking(user_number){
+  if(final_number == user_number){
+    display_el.textContent = "You got it correct!"
+  }else if(user_number < final_number){
+    if(user_number < final_number && user_number >= final_number-5){
+      display_el.textContent = "you are too close!, guess a little greater number"
+    }else{
+    display_el.textContent = "Guess a greater number"
+  }
+  }else if(user_number > final_number){
+    if(user_number >final_number && user_number <=final_number+5){
+      display_el.textContent = "You are too close!, guess a little smaller number"
+    }else{
+      display_el.textContent = "Guess a lesser number"
+    }
+  }
+}
+
+  
+  function enter_btn(){
+  let user_number = document.getElementById("number").value
+  if(chance >0){
+  checking(user_number)
+  chance--;
+  
+  chance_el.textContent = chance_text + chance
+  } 
+    
+  }
+  
+function start_again(){
+  chance = 11
+    enter_btn()
+  
+}
