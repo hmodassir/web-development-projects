cf201e0e69171972abba18a7805a4bfd8e133536
modassir_hussain <89156644+hmodassir@users.noreply.github.com>
2022-02-03 12:00:54 +0530

    Update guessNumber.js

--- Guess-the-number/guessNumber.js
+++ Guess-the-number/guessNumber.js
@@ -1,14 +1,21 @@
 let final_number = Math.floor(Math.random()*100)+1
+let final_number_display = document.getElementById("final_number_display")
 let display_el = document.getElementById("display")
 let chance_el = document.getElementById("user_chance")
 let chance = 10
 let chance_text = "Chance Left: "
 chance_el.textContent = chance_text + chance
-
+let gameOver = false 
 
 
 function checking(user_number){
-  if(final_number == user_number){
+  
+  if(user_number == 0){
+    display_el.textContent= "Please enter a number"
+  }
+  else if(final_number == user_number){
+    gameOver = true
+ 
     display_el.textContent = "You got it correct!"
   }else if(user_number < final_number){
     if(user_number < final_number && user_number >= final_number-5){
@@ -28,17 +35,25 @@ function checking(user_number){
   
   function enter_btn(){
   let user_number = document.getElementById("number").value
+   if(chance<=1){
+   final_number_display.textContent="The Number Was "+ final_number
+  }
   if(chance >0){
   checking(user_number)
   chance--;
   
   chance_el.textContent = chance_text + chance
-  } 
+  }
     
   }
   
 function start_again(){
-  if(chance == 0){
+  if(gameOver == true || chance === 0){
+  final_number_display.textContent= '';
+  display_el.value='';
+  document. getElementById("number").value=null
+  
+ final_number = Math.floor(Math.random()*100)+1
   chance = 11
     enter_btn()}
   
