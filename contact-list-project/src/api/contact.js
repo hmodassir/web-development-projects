530b88517acb11d59ba09698c5d71e2059945274
modassir_hussain <89156644+hmodassir@users.noreply.github.com>
2023-03-24 10:57:47 +0530

    Add files via upload

---
+++ contact-list-project/src/api/contact.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseURL:"http://localhost:3006",
+})
+https://replit.com/@roronoazoro24/React-Javascript-2?mobileWebview=1&mobileBridge=1&hideBottomBar=1&forceTheme=replitDark&fun=1#AddContact.js
